5b796367ee6f651e36ac647df33a2faa64a27057
jebozlesa <oravecmatej8@gmail.com>
2023-07-05 12:27:08 +0200

    playfab pridany

---
+++ Assets/Scripts/PlayFabManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class PlayFabManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Login();
+    }
+
+    void Login()
+    {
+        var request = new LoginWithCustomIDRequest 
+        {
+            CustomId = SystemInfo.deviceUniqueIdentifier,
+            CreateAccount = true
+        };
+        PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError);
+    }
+
+    void OnSuccess(LoginResult result)
+    {
+        Debug.Log("Sicko dobre");
+    }
+
+    void OnError(PlayFabError error)
+    {
+        Debug.Log("Sicko dobre");
+        Debug.Log(error.GenerateErrorReport());
+    }
+}
